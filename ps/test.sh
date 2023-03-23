f3f83032fe27069e892c74bf53ad0e722617eb7a
Bubbler-4 <bubbler9903@gmail.com>
2023-03-23 08:23:58 +0000

    Fix test script

--- ps/test.sh
+++ ps/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cargo oj
-if cargo boj test $1 && [ "$2" = 'go' ]; then
+cargo boj test $1 && if [ "$2" = 'go' ]; then
     cargo boj submit $1
 else
     echo 'Test successful, but not submitted.'
