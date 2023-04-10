15af3a79bddfbb931b20e06999981f65b80ae2fa
Bubbler-4 <bubbler9903@gmail.com>
2023-04-10 05:14:38 +0000

    add geometry lib

---
+++ ps/src/geometry.rs
@@ -0,0 +1,57 @@
+use std::cmp::{Ordering, Ordering::*};
+
+#[derive(Clone, Copy)]
+pub(crate) struct Frac(pub(crate) i64, pub(crate) i64);
+impl PartialEq for Frac {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 * other.1 == self.1 * other.0
+    }
+}
+impl Eq for Frac {}
+impl PartialOrd for Frac {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        (self.0 * other.1).partial_cmp(&(self.1 * other.0))
+    }
+}
+impl Ord for Frac {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.0 * other.1).cmp(&(self.1 * other.0))
+    }
+}
+
+/// (x, y) pts -> (upper hull, lower hull)
+pub(crate) fn convex_hull(pts: &[(i64, i64)]) -> (Vec<(i64, i64)>, Vec<(i64, i64)>) {
+    let mut v = pts.to_vec();
+    v.sort_unstable(); // min x; min y
+    let mut lower = vec![v[0]];
+    let mut upper = vec![v[0]];
+    for &(x, y) in &v[1..] {
+        'lower: {
+            while lower.len() >= 2 {
+                let (x2, y2) = lower[lower.len() - 1];
+                let (x1, y1) = lower[lower.len() - 2];
+                let prod = (x2 - x1) * (y - y2) - (x - x2) * (y2 - y1);
+                match prod.cmp(&0) {
+                    Less => { lower.pop(); }
+                    Equal => { lower.pop(); lower.push((x, y)); break 'lower; }
+                    Greater => { lower.push((x, y)); break 'lower; }
+                }
+            }
+            if x > lower[0].0 { lower.push((x, y)); }
+        }
+        'upper: {
+            while upper.len() >= 2 {
+                let (x2, y2) = upper[upper.len() - 1];
+                let (x1, y1) = upper[upper.len() - 2];
+                let prod = (x2 - x1) * (y - y2) - (x - x2) * (y2 - y1);
+                match prod.cmp(&0) {
+                    Less => { upper.push((x, y)); break 'upper; }
+                    Equal => { upper.pop(); upper.push((x, y)); break 'upper; }
+                    Greater => { upper.pop(); }
+                }
+            }
+            upper.push((x, y));
+        }
+    }
+    (upper, lower)
+}
--- ps/src/lib.rs
+++ ps/src/lib.rs
@@ -1,22 +1,45 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 use std::collections::*;
-use std::cmp::{Reverse, Ordering::{self, *}};
+use std::cmp::{Reverse, Ordering, Ordering::*};
 
 #[allow(clippy::all)]
 #[allow(unused_must_use)]
 fn solve<R: BufRead, W: Write>(ii: &mut I<R>, oo: &mut W) -> Option<()> {
-    let [t, s] = ii.get([0usize; 2])?;
-    match (t, s) {
-        (12..=16, 0) => writeln!(oo, "320"),
-        _ => writeln!(oo, "280"),
-    };
+    let t = ii.get(0usize)?;
+    for _ in 0..t {
+        let n = ii.get(0usize)?;
+        let mut v = vec![];
+        for i in 0..n {
+            v.push((ii.get(0i64)?, ii.get(0i64)?, i));
+        }
+        let xsum = v.iter().map(|x| x.0).sum::<i64>();
+        let ysum = v.iter().map(|y| y.1).sum::<i64>();
+        for i in 0..n {
+            v[i].0 = v[i].0 * n as i64 - xsum;
+            v[i].1 = v[i].1 * n as i64 - ysum;
+        }
+        use geometry::Frac;
+        v.sort_unstable_by_key(|pt| {
+            let &(x, y, _) = pt;
+            if x == 0 && y == 0 { (0, Frac(0, 1), 0) }
+            else if x > 0 && y >= 0 { (1, Frac(y, x), x+y) }
+            else if x <= 0 && y > 0 { (2, Frac(-x, y), y-x) }
+            else if x < 0 && y <= 0 { (3, Frac(-y, -x), -x-y) }
+            else { (4, Frac(x, -y), x-y) }
+        });
+        for &(_, _, i) in &v {
+            write!(oo, "{} ", i);
+        }
+        writeln!(oo);
+    }
     None
 }
 
 mod prime;
 mod ntheory;
 mod graph;
+mod geometry;
 /// IO template
 mod io;
 use io::*;
