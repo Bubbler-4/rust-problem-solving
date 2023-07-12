1e3cb048dd9101139373507beee935fbdc2415fe
Bubbler <bubbler9903@gmail.com>
2023-07-12 01:36:02 +0000

    found a use for u64 output

--- ps/src/io.rs
+++ ps/src/io.rs
@@ -53,7 +53,7 @@ macro_rules! print_disp {
     }
 }
 print_disp!(usize, i64, String, &str, char);
-print_disp!(u16, u32, u128);
+print_disp!(u16, u32, u64, u128);
 print_disp!(i16, i32, i128);
 impl Print for (f64, usize) {
     fn print<W: Write>(&self, w: &mut W) {
