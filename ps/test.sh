fef0f92272b77ae3d47cbb5b8f733d368f365f9e
Bubbler-4 <bubbler9903@gmail.com>
2023-11-21 06:48:34 +0000

    update cargo-boj; add -p to autosubmit

--- ps/test.sh
+++ ps/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cargo oj
-cargo boj test $1 && if [ "$2" = 'go' ]; then
+cargo boj test $1 -p && if [ "$2" = 'go' ]; then
     cargo boj submit $1
 else
     echo 'Test successful, but not submitted.'
